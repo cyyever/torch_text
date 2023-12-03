6235e2634ced9d670f9d4a4bd395d9fae2976e47
cyy <cyyever@outlook.com>
2023-12-03 11:21:23 +0800

    Add code

--- cyy_torch_text/data_pipeline/__init__.py
+++ cyy_torch_text/data_pipeline/__init__.py
@@ -5,12 +5,11 @@ from .transform import add_text_extraction, add_text_transforms
 
 
 def append_transforms_to_dc(dc, model_evaluator=None) -> None:
-    if dc.dataset_type == DatasetType.Text:
-        if model_evaluator is None:
-            add_text_extraction(dc=dc)
-        else:
-            add_text_transforms(dc=dc, model_evaluator=model_evaluator)
-        return
+    if model_evaluator is None:
+        add_text_extraction(dc=dc)
+    else:
+        add_text_transforms(dc=dc, model_evaluator=model_evaluator)
+    return
 
 
 global_data_transform_factory.register(DatasetType.Text, append_transforms_to_dc)
