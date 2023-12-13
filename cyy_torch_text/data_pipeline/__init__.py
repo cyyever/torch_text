b7e400abf75cd22d40bf6750bd404d5df7776954
cyy <cyyever@outlook.com>
2023-12-13 10:47:39 +0800

    Fix code

--- cyy_torch_text/data_pipeline/__init__.py
+++ cyy_torch_text/data_pipeline/__init__.py
@@ -9,7 +9,6 @@ def append_transforms_to_dc(dc, model_evaluator=None) -> None:
         add_text_extraction(dc=dc)
     else:
         add_text_transforms(dc=dc, model_evaluator=model_evaluator)
-    return
 
 
 global_data_transform_factory.register(DatasetType.Text, append_transforms_to_dc)
