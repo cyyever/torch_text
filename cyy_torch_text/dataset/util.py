df4ed06c013a920cd99e4f8164950a000affab3c
cyy <cyyever@outlook.com>
2024-03-10 22:29:17 +0800

    Add code

--- cyy_torch_text/dataset/util.py
+++ cyy_torch_text/dataset/util.py
@@ -5,5 +5,9 @@ from cyy_torch_toolbox import DatasetUtil
 
 class TextDatasetUtil(DatasetUtil):
     @torch.no_grad()
-    def get_sample_text(self, index: int) -> str:
-        return self._get_sample_input(index, apply_transform=False)
+    def get_sample_text(self, index: int, apply_transform: bool = False) -> str:
+        sample_text = self._get_sample_input(index=index, apply_transform=False)
+        if apply_transform:
+            assert self._transforms is not None
+            sample_text = self._transforms.transform_text(sample_text)
+        return sample_text
