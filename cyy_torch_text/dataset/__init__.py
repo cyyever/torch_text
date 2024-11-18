518e5a3e0ce141266467392b6bb0eca4754d3107
cyy <cyyever@outlook.com>
2024-11-18 18:22:25 +0800

    Fix port

--- cyy_torch_text/dataset/__init__.py
+++ cyy_torch_text/dataset/__init__.py
@@ -2,7 +2,6 @@ import cyy_huggingface_toolbox.dataset  # noqa: F401
 from cyy_torch_toolbox import DatasetType
 from cyy_torch_toolbox.dataset.util import global_dataset_util_factor
 
-from .collection import TextDatasetCollection  # noqa: F401
 from .util import TextDatasetUtil
 
 global_dataset_util_factor.register(DatasetType.Text, TextDatasetUtil)
--- cyy_torch_text/dataset/collection.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from cyy_torch_toolbox import DatasetCollection, DatasetType
-
-
-class TextDatasetCollection(DatasetCollection):
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-        assert self.dataset_type == DatasetType.Text
