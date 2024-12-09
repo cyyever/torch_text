bd100231982d7696e85c852387fde891f7833843
cyy <cyyever@outlook.com>
2024-12-09 10:43:51 +0800

    Fix code

--- cyy_torch_text/dataset/__init__.py
+++ cyy_torch_text/dataset/__init__.py
@@ -1,7 +1,1 @@
 import cyy_huggingface_toolbox.dataset  # noqa: F401
-from cyy_torch_toolbox import DatasetType
-from cyy_torch_toolbox.dataset.util import global_dataset_util_factor
-
-from .util import TextDatasetUtil
-
-global_dataset_util_factor.register(DatasetType.Text, TextDatasetUtil)
