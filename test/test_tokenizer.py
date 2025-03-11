39816229fd1f98ce84e63bea670899e48d4cac60
cyy <cyyever@outlook.com>
2025-03-11 10:13:20 +0800

    Fix code

--- test/test_tokenizer.py
+++ test/test_tokenizer.py
@@ -1,8 +1,7 @@
 import os
 
 from cyy_torch_text.model import TextModelEvaluator
-from cyy_torch_toolbox import Config, Executor, Tokenizer
-from cyy_torch_toolbox.tokenizer import convert_phrase_to_token_ids
+from cyy_torch_toolbox import Config
 
 has_spacy: bool = False
 try:
@@ -15,7 +14,7 @@ except:
 if has_spacy:
     os.environ["USE_THREAD_DATALOADER"] = "1"
 
-    def tokenizer_testcases(executor: Executor, tokenizer: Tokenizer) -> None:
+    def tokenizer_testcases(tokenizer: SpacyTokenizer) -> None:
         phrase = "hello world!"
         tokens = tokenizer.tokenize(phrase=phrase)
         assert tokens == ["hello", "world", "!"]
@@ -23,7 +22,6 @@ if has_spacy:
             token_id = tokenizer.get_token_id(token)
             recovered_token = tokenizer.get_token(token_id)
             assert recovered_token == token
-        convert_phrase_to_token_ids(executor=executor, phrase=phrase)
 
     def test_spacy_tokenizer() -> None:
         config = Config(dataset_name="imdb", model_name="simplelstm")
@@ -31,4 +29,4 @@ if has_spacy:
         assert isinstance(trainer.model_evaluator, TextModelEvaluator)
         tokenizer = trainer.model_evaluator.tokenizer
         assert isinstance(tokenizer, SpacyTokenizer)
-        tokenizer_testcases(executor=trainer, tokenizer=tokenizer)
+        tokenizer_testcases(tokenizer=tokenizer)
